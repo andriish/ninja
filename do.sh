8a74700df72105465520fef9b645d55a79ca48b4
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.d>
2025-10-14 18:18:10 +0200

    OK

--- do.sh
+++ do.sh
@@ -4,7 +4,7 @@
 #bear --output make.json -- make -j 10
 #make install 
 rm -rf BUILD
-cmake -S . -B BUILD -DAVHOLO=OFF -DF90MODULE=ON  -DGOSAM=ON -DQUADNINJA=ON -DCMAKE_INSTALL_PREFIX=$(pwd)/CINSTALL -DCMAKE_INSTALL_LIBDIR=lib
+cmake -S . -B BUILD -DAVHOLO=OFF -DF90MODULE=OFF  -DGOSAM=OFF -DQUADNINJA=ON -DCMAKE_INSTALL_PREFIX=$(pwd)/CINSTALL -DCMAKE_INSTALL_LIBDIR=lib
 bear --output cmake.json -- cmake --build BUILD -j 10
 cmake --install BUILD
 ./file_differences.py  cmake.json make.json cm.txt m.txt
