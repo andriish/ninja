5161c2be2f8dc70b991f9194b5d456f2fb8c4a2b
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.d>
2025-10-14 17:28:20 +0200

    OK

--- do.sh
+++ do.sh
@@ -1,8 +1,10 @@
-make clean
-autoreconf -i 
-./configure  --with-avholo=no --enable-quadninja=no --disable-f90module --with-quadruple=no
-bear --output make.json -- make 
+#make clean
+#autoreconf -i 
+#./configure  --prefix=$(pwd)/MINSTALL --with-avholo=no --enable-quadninja=yes  --with-quadruple=no
+#bear --output make.json -- make -j 10
+#make install 
 rm -rf BUILD
-cmake -S . -B BUILD -DAVHOLO=OFF
-bear --output cmake.json -- cmake --build BUILD
+cmake -S . -B BUILD -DAVHOLO=OFF -DF90MODULE=ON  -DGOSAM=ON -DQUADNINJA=ON -DCMAKE_INSTALL_PREFIX=$(pwd)/CINSTALL -DCMAKE_INSTALL_LIBDIR=lib
+bear --output cmake.json -- cmake --build BUILD -j 10
+cmake --install BUILD
 ./file_differences.py  cmake.json make.json cm.txt m.txt
